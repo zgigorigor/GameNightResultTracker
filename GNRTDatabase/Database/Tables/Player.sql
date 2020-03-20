392aa4ca268aaaac1bf647c48659df36a5568b93
Igor, PC <bistrovic.igor@gmail.com>
2020-03-20 14:26:37 +0100

    created database

---
+++ GNRTDatabase/Database/Tables/Player.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Player]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Game] NCHAR(10) NOT NULL, 
+    [Record] INT NOT NULL, 
+    [Date] DATE NOT NULL
+)
